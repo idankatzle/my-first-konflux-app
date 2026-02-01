e5ffa085843fae4710c776b38d824a4d6dd5c28e
idankatzle <t0506014365@gmail.com>
2026-02-01 10:59:11 +0200

    app.sh index.html

--- app.sh
+++ app.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-while true; do
-  echo "Hello from Konflux"
-  sleep 10
-done
+#!/bin/bash
+echo "Starting Map Application on port 8080..."
+# Python simple HTTP server will serve index.html by default
+python3 -m http.server 8080
